79bf8d854ea4a4793d1f3f21db9af90483e54134
MohammedAtallahhh <72510578+MohammedAtallahhh@users.noreply.github.com>
2022-10-30 22:56:11 +0000

    Initial setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fast Feedback</title>
+      </Head>
+
+      <h1>Hello world!</h1>
+    </>
+  );
+}
